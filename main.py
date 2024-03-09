118223cac873349e07ce2a307f87c0b0f4d810de
Valentin Marta <valenmarta17@gmail.com>
2024-03-09 12:17:52 -0300

    creating a menu for the computer manager

---
+++ main.py
@@ -0,0 +1,27 @@
+def main():
+    
+    while True:
+
+        op = input("""
+                    1.   Create computer.
+                    2.   Present computer.
+                    3.   Change the computer OS.
+                    4.   Show a list of peripheral.
+                    5.   Exit.
+                    option: """)
+        
+        if op == "1":
+            pass
+        elif op == "2":
+            pass
+        elif op == "3":
+            pass
+        elif op == "4":
+            pass
+        elif op == "5":
+            break
+        else:
+            print("invalid value, try again")
+
+if __name__ == "__main__":
+    main()
